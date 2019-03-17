90b83c0400f49bed314e28f856dc1b4e39c08a63
nmerk <nadyamerk@gmail.com>
2019-03-17 23:46:32 +0300

    tree finished, tests pass

--- tree/tree.go
+++ tree/tree.go
@@ -6,60 +6,94 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 )
 
-func makeTreeBranch(level int, last os.FileInfo, element os.FileInfo) string {
-	var stringsBuilder strings.Builder
-	for i := 0; i < level; i++ {
-		fmt.Print("|")
+type directoryContent []os.FileInfo
 
-		fmt.Print(&stringsBuilder, "\t")
-	}
-	if element == last {
-		fmt.Print("└--")
-	} else {
-		fmt.Print("├--")
-	}
-	return stringsBuilder.String()
+func (content directoryContent) Len() int { return len(content) }
+func (content directoryContent) Less(i int, j int) bool { return content[i].Name() < content[j].Name() }
+func (content directoryContent) Swap(i int, j int) {
+	temp := content[i]
+	content[i] = content[j]
+	content[j] = temp
 }
 
-func showDir(out io.Writer, path string, printFiles bool, level int) error {
+func makeTreeBranch(level int, last os.FileInfo, element os.FileInfo, mas *[]bool) string {
+	var branch strings.Builder
+
+	for i := 0; i < level; i++ {
+		if (*mas)[i] == true {
+			branch.WriteString( "│")
+		}
+		branch.WriteString("\t")
+	}
+
+	if element == last {
+		branch.WriteString("└───")
+		(*mas)[level] = false
+	} else {
+		branch.WriteString("├───")
+	}
+
+	branch.WriteString(element.Name())
+
+	if element.IsDir() {
+		branch.WriteString("\n")
+	}
+
+	return branch.String()
+}
+
+func fileSize(element os.FileInfo) string {
+	if element.Size() == 0 {
+		return " (empty)\n"
+	} else {
+		return " (" + strconv.FormatInt(element.Size(), 10) + "b)\n"
+	}
+}
+
+func showDir(out io.Writer, path string, printFiles bool, level int, mas []bool) error {
+	var dirContent directoryContent
 	dirContent, err := ioutil.ReadDir(path)
 
 	if err != nil {
 		return errors.New("can't read dir")
 	}
 
-	// remove files
-	if !printFiles {
-		for i := 0; i < len(dirContent); i++ {
-			if !dirContent[i].IsDir() {
-				dirContent = append(dirContent[:i], dirContent[i+1:]...)
-				i--
-			}
+	sort.Sort(dirContent)
+	if len(dirContent) != 0 {
+		if !printFiles && !dirContent[len(dirContent)-1].IsDir() {
+			dirContent = dirContent[:len(dirContent)-1]
 		}
 	}
-
 
 	for _, element := range dirContent {
 		if element.IsDir() {
-			fmt.Println(makeTreeBranch(level, dirContent[len(dirContent) - 1], element), element.Name())
-			showDir(out, path + "/" + element.Name(), printFiles, level + 1)
-		} else {
-			fmt.Print(makeTreeBranch(level, dirContent[len(dirContent) - 1], element), element.Name())
-			if element.Size() == 0 {
-				fmt.Print(" (empty)\n")
-			} else {
-				fmt.Print(" (", element.Size(), "b)\n")
+			if len(mas) <= level + 1 {
+				mas = append(mas, false)
+				mas[level] = true
 			}
+
+			fmt.Fprint(out, makeTreeBranch(level, dirContent[len(dirContent) - 1], element, &mas))
+
+			if err := showDir(out, path + "/" + element.Name(), printFiles, level + 1, mas); err != nil {
+				return err
+			}
+		} else if !element.IsDir() && printFiles {
+			fmt.Fprint(out, makeTreeBranch(level, dirContent[len(dirContent) - 1], element, &mas), fileSize(element))
 		}
 	}
+
+	mas[level] = false
+
 	return nil
 }
 
 func dirTree(out io.Writer, path string, printFiles bool) error {
-	initialLevel := 0
-
-	return showDir(out, path, printFiles, initialLevel)
+	mas := make([]bool, 0, 1)
+	mas = append(mas, true)
+	return showDir(out, path, printFiles, 0, mas)
 }
